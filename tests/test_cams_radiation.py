cfa5f6c4175a447422986df0221cab9997430f9e
Olli Nevalainen <olli.nevalainen.fmi@gmail.com>
2024-11-12 12:50:42 +0200

    Relaxed dependencies, updated test

--- tests/test_cams_radiation.py
+++ tests/test_cams_radiation.py
@@ -1,6 +1,9 @@
+import os
+from cams_radiation.cams_radiation import CDSRequest
+
+
 class TestQvidja:
     def test_request_qvidja_daily(self):
-        from cams_radiation.cams_radiation import CDSRequest
 
         lat = 60.294264172757536
         lon = 22.390893932124552
@@ -9,7 +12,7 @@ class TestQvidja:
         end_time = "2023-12-05"
         time_step = "1day"
         output_format = "csv"
-
+        cdsapi_kwargs = check_environ_cdsapi_configuration()
         cds_request = CDSRequest(
             latitude=str(lat),
             longitude=str(lon),
@@ -18,6 +21,19 @@ class TestQvidja:
             end_time=end_time,
             time_step=time_step,
             output_format=output_format,
+            cdsapi_kwargs=cdsapi_kwargs,
         )
 
         df = cds_request.send_request()
+
+
+def check_environ_cdsapi_configuration():
+    """Function for checking if CDSAPI configuration envionment variables are set. Used in tests."""
+    # Check if CDSAPI configuration envionment variables are set
+    cdsapi_key = os.environ.get("CDSAPI_KEY_PYTEST")
+    cdsapi_url = os.environ.get("CDSAPI_URL_PYTEST")
+    if cdsapi_key is not None and cdsapi_url is not None:
+        cdsapi_kwargs = {"key": cdsapi_key, "url": cdsapi_url}
+    else:
+        cdsapi_kwargs = None
+    return cdsapi_kwargs
